fa1dac11c373bc9cc3d0920c5bbe11ae0f4f2f79
jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-08-27 16:03:47 -0300

    ex2
    ex2

--- Exercícios/js/ex01.js
+++ Exercícios/js/ex01.js
@@ -5,3 +5,6 @@ numero=parseInt(prompt("Digite um numero"))
 if(numero <3){
     alert("O triplo desse numero é: "+numero*3)
 }
+else{
+    alert("O número é inválido")
+}
---
+++ Exercícios/js/ex02.js
@@ -0,0 +1,10 @@
+var numero
+
+numero=parseInt(prompt("Digite um numero"))
+
+if(numero <3){
+    alert("O dobro desse numero é: "+numero*2)
+}
+else{
+    alert("O triplo desse numero é: "+numero*3)
+}
