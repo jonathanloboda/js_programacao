637babc5f54a23ea3ed3ceae4eff5a76712b97b3
jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-09-03 15:40:53 -0300

    exadivinhação
    exadivinhação

---
+++ Exercícios/js/exadivinhacao.js
@@ -0,0 +1,17 @@
+const numeroAleatorio = Math.floor(Math.random() * 101);
+console.log(numeroAleatorio);
+
+var numero
+
+numero=parseInt(prompt(numeroAleatorio+"Tente adivinhar o número:"))
+
+while(numero != numeroAleatorio){
+    if(numero < numeroAleatorio){
+        alert("O numero digitado é menor que o correto.")
+    }
+    if(numero > numeroAleatorio){
+        alert("O numero digitado é maior que o correto.")
+    }
+    numero=parseInt(prompt(numeroAleatorio+"Tente adivinhar o número:"))
+}
+alert("Você acertou!!!")
