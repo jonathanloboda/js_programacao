64a8e77678ad557f58b25f87521e57fd75e6b609
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-10-15 20:08:40 -0300

    ex03
    Mudando a cor

---
+++ dom/js/ex02.js
@@ -0,0 +1,23 @@
+function calcular()
+{
+    var v1, v2, r
+    v1 = parseFloat(document.getElementById("peso").value);
+    v2 = parseFloat(document.getElementById("altura").value);
+    r = v1 / (v2 * v2);
+    document.getElementById("resultado").innerHTML = "Seu IMC é: " + r.toFixed(2);
+    if (r < 18.5)
+    {
+        document.getElementById("situacao").innerHTML = "Situação: Abaixo do peso";
+        document.getElementById("situacao").style.color = "red";
+    }
+    else if (r >= 18.5 && r < 25)
+    {
+        document.getElementById("situacao").innerHTML = "Situação: Peso normal";
+        document.getElementById("situacao").style.color = "green";
+    }
+    else if (r >= 25 && r < 40)
+    {
+        document.getElementById("situacao").innerHTML = "Situação: Sobrepeso";
+        document.getElementById("situacao").style.color = "red";
+    }
+}
