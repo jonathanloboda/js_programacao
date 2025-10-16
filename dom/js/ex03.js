5d3651d1e0aa585604859e3825b6afa3ddb949f1
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-10-15 22:26:17 -0300

    Update ex03.js

--- dom/js/ex03.js
+++ dom/js/ex03.js
@@ -3,7 +3,7 @@ function entrar() {
     var senha = document.getElementById("senha").value;
 
     if (usuario === "jonathan123" && senha === "1234") {
-        document.getElementById("mensagemdeentrada").innerHTML = "<p style='color: green;'>Login bem-sucedido! Bem-vindo, " + usuario + ".</p>";
+        document.getElementById("mensagemdeentrada").innerHTML = "<p style='color: white;'>Login bem-sucedido! Bem-vindo, " + usuario + ".</p>";
     } else {
         document.getElementById("mensagemdeentrada").innerHTML = "<p style='color: red;'>Usuário ou senha incorretos. Tente novamente.</p>";
     }
