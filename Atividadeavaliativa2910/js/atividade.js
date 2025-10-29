17bd9d3bab16db27185f215c46a67f2982e92c95
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-10-29 18:01:15 -0300

    js atualizado

--- Atividadeavaliativa2910/js/atividade.js
+++ Atividadeavaliativa2910/js/atividade.js
@@ -6,7 +6,7 @@ function urgente()
 {
     var novanota, nota
     novanota=document.createElement("li")
-    novanota.id="novanota"
+    novanota.id="listaurgente"
     novanota.innerHTML=nota=document.getElementById("nota").value
     var div=document.getElementById("urgente")
     div.appendChild(novanota)
@@ -15,33 +15,27 @@ function naourgente()
 {
     var novanota, nota
     novanota=document.createElement("li")
-    novanota.id="novanota"
+    novanota.id="listanaourgente"
     novanota.innerHTML=nota=document.getElementById("nota").value
     var div=document.getElementById("naourgente")
     div.appendChild(novanota)
 }
 function removerurg()
 {
-    var li=document.getElementById("novanota")
-    if(li)
-    {
+    var li=document.getElementById("listaurgente")
+    if(li){
         li.remove()
-    }
-    else
-    {
-        alert("Não há notas para remover.")
+    }else{
+    alert("Não há texto para remover")    
     }
 }
 function removernurg()
 {
-    var li=document.getElementById("novanota")
-    if(li)
-    {
+    var li=document.getElementById("listanaourgente")
+    if(li){
         li.remove()
-    }
-    else
-    {
-        alert("Não há notas para remover.")
+    }else{
+    alert("Não há texto para remover")    
     }
 }
 function removertudo()
