aa9daf2e8b784a42442ff39646d3b414374700b5
jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-10-29 15:45:49 -0300

    js implementado sem radio
    js implementado sem radio

---
+++ Atividadeavaliativa2910/js/atividade.js
@@ -0,0 +1,56 @@
+var nota=document.getElementById("nota").value
+
+
+
+function urgente()
+{
+    var novanota, nota
+    novanota=document.createElement("li")
+    novanota.id="novanota"
+    novanota.innerHTML=nota=document.getElementById("nota").value
+    var div=document.getElementById("urgente")
+    div.appendChild(novanota)
+}
+function naourgente()
+{
+    var novanota, nota
+    novanota=document.createElement("li")
+    novanota.id="novanota"
+    novanota.innerHTML=nota=document.getElementById("nota").value
+    var div=document.getElementById("naourgente")
+    div.appendChild(novanota)
+}
+function removerurg()
+{
+    var li=document.getElementById("novanota")
+    if(li)
+    {
+        li.remove()
+    }
+    else
+    {
+        alert("Não há texto para remover.")
+    }
+}
+function removernurg()
+{
+    var li=document.getElementById("novanota")
+    if(li)
+    {
+        li.remove()
+    }
+    else
+    {
+        alert("Não há texto para remover.")
+    }
+}
+function removertudo()
+{
+    var li=document.getElementById("urgente")
+    li.innerHTML=""
+}
+function removertudon()
+{
+    var li=document.getElementById("naourgente")
+    li.innerHTML=""
+}
