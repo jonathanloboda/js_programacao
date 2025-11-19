7ace19d22003959ff83e0f8251497d6d46fd95c8
Jonathan8 <jonathan.loboda.ifpr.2023@gmail.com>
2025-11-19 15:43:23 -0300

    alteração de espaçamento

--- eventos/js/eventos.js
+++ eventos/js/eventos.js
@@ -3,23 +3,35 @@ function exibir()
 {
     alert("Manipulação direta do evento!")
 }
+
+
+
 /*Exemplo 2:*/
 function exibir_anonima()
 {
     let botao=document.getElementById("btn1")
     botao.addEventListener("click", function(){ alert("A função anônima é uma função sem nome exemplo: function(){...}. Pode ser chamad a de função anônima, callback, Função de Evento (Event Handler)")})
 }
+
+
+
 function exibir_nominada()
 {
     let botao=document.getElementById("btn2")
     /*Chamando a função a partir do click*/
     botao.addEventListener("click", exibir)
 }
+
+
+
 function exibir_flecha()
 {
     let botao=document.getElementById("btn3")
     botao.addEventListener("click", ()=> {alert("A função arrow é dada por ()=>(...)")})
 }
+
+
+
 /*Exemplo 3 - Ativando o evento com a passagem do mouse */
 function mudar_cor()
 {
@@ -31,6 +43,8 @@ function mudar_cor()
         mouse.innerText="Evento com o mouse"
     }) 
 }
+
+
 /*As funções devem ser chamadas para ativar o listener assim que a página carregar */
 exibir_anonima()
 exibir_nominada()
